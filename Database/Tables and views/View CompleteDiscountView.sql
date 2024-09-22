d4b758662fa228b21160b122a1bc6cd01ba893de
Pablo Agustin Vieyra <104534435+AgustinVieyra@users.noreply.github.com>
2024-09-21 18:04:33 -0600

    Adding views
    Adding a more descriptive view of the slowly changing dimensions table of Dicounts of the bought books. And Invoice and Invoice Details viewm that report on the books bought as a subtotal, their lowered price, and the total amount pewr transaction.

---
+++ Database/Tables and views/View CompleteDiscountView.sql
@@ -0,0 +1,15 @@
+CREATE VIEW [CompleteDiscountView] AS
+
+--Without the DISTINCT fucntion in this CTE, it creates a Cartesian Product for the view
+WITH BoughtBooks AS (
+    SELECT DISTINCT [ISBN]
+    FROM TransactionDetails
+)
+
+SELECT BB.[ISBN], 
+    IFNULL(D.[Discount_pct], 0) AS Discount_pct,
+    COALESCE(D.[Start_date], '1970-01-01') AS Start_date,
+    COALESCE(D.[End_date], DATE('now')) AS End_date
+FROM BoughtBooks BB
+LEFT JOIN Discounts D
+    ON D.[ISBN] = BB.[ISBN];
