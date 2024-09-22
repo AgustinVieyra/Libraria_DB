5ec81661b550b80890797d348b995c011dc5770a
Pablo Agustin Vieyra <104534435+AgustinVieyra@users.noreply.github.com>
2024-09-21 21:55:37 -0600

    Development (#6)
    * Adding views
    
    Adding a more descriptive view of the slowly changing dimensions table of Dicounts of the bought books. And Invoice and Invoice Details viewm that report on the books bought as a subtotal, their lowered price, and the total amount pewr transaction.
    
    * Change from 'Create <Name_of_table>' to 'Table  <Name_of_table>'
    
    Changes the names of the create table sql files from 'Create <Name_of_table>.sql' to 'Table  <Name_of_table>.sql'
    
    * More views
    
    Adding a view of the sales in the last two months (by user and in general). As well as an extra view of the avaliable data of the translated books xoalesced with the data in the original language (froom the Books table).

---
+++ Database/Tables and views/View SalesByGenreLastTwoMonths.sql
@@ -0,0 +1,12 @@
+CREATE VIEW [SalesByGenreLastTwoMonths] AS
+SELECT Genre, STRFTIME('%m',Transaction_date) AS Month,
+CASE
+    WHEN STRFTIME('%m',Transaction_date) = STRFTIME('%m',DATE('now')) THEN 'Current_month'
+    WHEN STRFTIME('%m',Transaction_date) = STRFTIME('%m',DATE('now','-1 month')) THEN 'Last_month'
+END AS Month_flag,
+    SUM(Number_of_items) 'Books sold', SUM(Subtotal_by_title) 'Subtotal by genre & month',
+    SUM((Discount_pct/100)*Subtotal_by_title) AS 'Discounted amount', SUM(Total_by_title) 'Sales by genre & month'
+FROM InvoiceDetails
+WHERE Month_flag IS not NULL
+GROUP BY Genre, Month_flag
+ORDER BY Month_flag, 'Books sold' DESC, Genre;
